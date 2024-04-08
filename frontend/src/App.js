0df934a5e3949992428e6243183a8296564854ca
Sephia Laureencia <hemas.laureencia@sjsu.edu>
2024-04-08 00:42:31 -0700

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>CS157C - Team 8</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
