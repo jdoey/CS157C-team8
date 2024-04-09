4b1f83de3133c7d87189fed57b8ba556ff2f2528
jdoey <70018021+jdoey@users.noreply.github.com>
2024-04-08 19:23:45 -0700

    node + express + mongodb connection

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const mongoString = process.env.DATABASE_URL
+const port = 3000;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+const routes = require('./src/routes/routes');
+
+app.use('/api', routes)
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
+
