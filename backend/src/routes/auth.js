f9b94859b9fb98c9f43d8b11d355c42488f3a1fa
jdoey <70018021+jdoey@users.noreply.github.com>
2024-04-11 01:26:45 -0700

    login route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -7,6 +7,24 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send('Hello World!'));
 
-router.get('/test', (req, res) => res.send('test success!'));  
+router.get('/test', (req, res) => res.send('test success!'));
+
+router.get('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await UserCredentials.findOne({ email });
+
+    if (!user) {
+        return res.status(401).json({ message: "Invalid email or password"});
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    
+    if (!isValidPassword) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    return res.status(200).json({ message: 'Login successful' });
+})
 
 module.exports = router;
