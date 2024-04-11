7b51d5564bcda3bc9c0fc0c89cd1265f927059c2
jdoey <70018021+jdoey@users.noreply.github.com>
2024-04-11 01:27:34 -0700

    backend port change to 3001

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 
 const mongoString = process.env.DATABASE_URL
-const port = 3000;
+const port = 3001;
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
