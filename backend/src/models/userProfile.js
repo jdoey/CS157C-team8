5227427f57c3344f6c058e4b35f1f5935bfd2229
jdoey <70018021+jdoey@users.noreply.github.com>
2024-04-11 01:07:37 -0700

    name update

--- backend/src/models/userProfile.js
+++ backend/src/models/userProfile.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userProfileSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'UserCredentials', required: true },
     name: String,
     location: String,
     bio: String,
