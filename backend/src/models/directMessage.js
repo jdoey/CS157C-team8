c549b0629a8470f38c5953900398702687d4883e
jdoey <70018021+jdoey@users.noreply.github.com>
2024-04-13 15:49:05 -0700

    data model for direct messages

---
+++ backend/src/models/directMessage.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const directMessageSchema = new mongoose.Schema({
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'UserProfile', required: true },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'UserProfile', required: true },
+    content: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+    updated_at: { type: Date }
+});
+
+const DirectMessage = mongoose.model('DirectMessage', directMessageSchema);
+
+module.exports = { DirectMessage };
