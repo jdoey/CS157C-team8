c8df24c2e2775ac8728f95db743d78ee1fbc4ac0
Sephia Laureencia <hemas.laureencia@sjsu.edu>
2024-05-04 23:07:47 -0700

    implement edit profile

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -25,4 +25,40 @@ router.get("/getProfile", checkLoggedIn, async (req, res) => {
   }
 });
 
+router.post("/updateUser", checkLoggedIn, async (req, res) => {
+  try {
+    const userId = req.session.user._id;
+    const user_id = new ObjectId(userId); // Use ObjectId to convert userId to ObjectId
+    const {
+      ownerName,
+      gender,
+      birthday,
+      city,
+      state,
+      ownerPrompts: ownerPrompts,
+      dogs: dogs,
+    } = req.body;
+
+    const result = await UserProfile.updateOne(
+      { user_id },
+      {
+        $set: {
+          ownerName: ownerName,
+          gender: gender,
+          birthday: birthday,
+          city: city,
+          state: state,
+          ownerPrompts: ownerPrompts,
+          dogs: dogs,
+        },
+      }
+    );
+    res.status(200).json({ message: "User info updated" });
+  } catch (err) {
+    console.log(err);
+    console.log(`ERROR UPDATING USER INFO: ${err}`);
+    res.status(500).json({ message: "ERROR UPDATING USER INFO" });
+  }
+});
+
 module.exports = router;
